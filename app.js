47fceee4a68b588f755d59161a0f0620af33eae0
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-09-21 11:01:03 -0700

    Add initial files

---
+++ app.js
@@ -0,0 +1,1 @@
+var suprStor = angular.module('suprStor', []);
---
+++ js/StoresController.js
@@ -0,0 +1,24 @@
+suprStor.controller('StorCtrl', function StorCtrl($scope) {
+  $scope.stores = [];
+
+  $scope.addStore = function() {
+    $scope.stores.push({
+      name: $scope.storeName,
+      type: $scope.storeType,
+      location: $scope.storeLocation,
+      squareFootage: $scope.squareFootage
+    });
+
+    // Clear input fields
+    $scope.name = null;
+    $scope.type = null;
+    $scope.location = null;
+    $scope.squareFootage = null;
+  };
+
+  $scope.deleteStore = function(store) {
+    var index = $scope.stores.indexOf(store);
+    $scope.stores.splice(index, 1);
+  };
+  
+});
